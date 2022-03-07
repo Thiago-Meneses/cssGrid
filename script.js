b325a32da8c4e225f2c35fa6aa4650a8e13d07e4
Thiago Meneses <thiagovieiramachado123@hotmail.com>
2022-03-07 15:12:14 -0300

    v1.0 first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+
+console.log(window.getComputedStyle(document.documentElement.children[1].children[0].children[1].children[0].children[1].children[3].children[0]).getPropertyValue('height'));
+
+console.log(document.documentElement.children[1].children[0].children[1].children[0].children[1].children[3].children[0]);
+
+
